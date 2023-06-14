93c2296d1feffeb41fad743db18d6b2cbd832d2e
Jorge Luis Reyes Señorellys <42388454+JorgeLuisReyes@users.noreply.github.com>
2023-06-14 11:33:54 -0400

    Problem

--- PruebaTecnica/Prueba1.js
+++ PruebaTecnica/Prueba1.js
@@ -1,1 +1,25 @@
-hola
+/*
+ * Problem 1 -
+ *
+ * Create an Object with a “hello” method that writes “Mello <name> in the console”
+ *
+ */
+
+/*
+ * Problem 1 - (B)
+ *
+ * How would you make the name imutable?
+ * (Can write or just describe)
+ *
+ */
+
+//T000 -->
+
+/*
+ *
+ * Problea 2
+ *
+ * Wrtie a function that logs the S cíties that occur the most ín the array below in
+ * order from the most number of occurrences to least.
+ *
+ */
